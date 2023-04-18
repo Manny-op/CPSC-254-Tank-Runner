5baf0e6a2b08c00ece2d3e106eae1a7dba3b52ca
Mario Linares <mariolinares@csu.fullerton.edu>
2023-04-18 09:24:10 -0700

    Add comments to the files for better clarification

--- tankrunner/game.py
+++ tankrunner/game.py
@@ -1,33 +1,8 @@
-"""File decleration"""
+"""This file contains the TankRunner class which builds and runs the game."""
 
 import pygame
 from tankrunner.window import WindowTitle
-"""
-class TankRunner:
-    This class represents the Tank Runner game
 
-    def __init__(self, window_with=800, window_height=600, window_title="Tank Runner", background_color=(156, 167, 184)):
-    "Initialization of the Tank Runner game
-        pygame.init()
-        self._window_size = (window_with, window_height)
-        self._clock = pygame.time.Clock()
-        self._screen = pygame.display.set_mode(self._window_size)
-        self._screen.fill(background_color)
-        self._title = window_title
-        pygame.display.set_caption(self._title)
-
-    def run(self):
-        A loop that will run the game.
-        running = True
-        while running:
-            for event in pygame.event.get():
-                pygame.display.update()
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    running = False
-        pygame.quit()
-        return 0
-
-"""
 
 class TankRunner:
     """This class represents the Tank Runner game."""
@@ -42,7 +17,6 @@ class TankRunner:
         pygame.display.set_caption(self._title)
         self._game_over = False
         self._windows = [WindowTitle("Tank Runner", self._window)]
-        
 
     def run(self):
         """A loop that runs the game."""
--- tankrunner/window.py
+++ tankrunner/window.py
@@ -1,8 +1,10 @@
+"""This file contains the Window class and its subclasses.These classes build the Windows of the game."""
 
 import pygame
 
+
 class Window:
-    """"""
+    """This class represents a TankRuner game Window."""
 
     def __init__(self, window):
         """Initialize the Window object."""
@@ -14,37 +16,44 @@ class Window:
         self._frame_rate = 60
 
     def update(self):
+        """Update the Window."""
         pass
 
     def draw(self):
-        self._window.blit(self._background, (0,0))
+        """Draw the window."""
+        self._window.blit(self._background, (0, 0))
 
     @property
     def frame_rate(self):
+        """Return the frame rate."""
         return self._frame_rate
-    
+
     @property
     def valid(self):
+        """Return weather a Window is valid."""
         return self._valid
-    
+
     def handle_event(self, event):
         """Trigger to move to next Window."""
         if event.type == pygame.KEYDOWN and \
-            event.key == pygame.K_ESCAPE :
+            event.key == pygame.K_ESCAPE:
             self._valid = False
 
 
 class WindowTitle(Window):
+    """A sublcass of Window that represents the Window title of TankRunner."""
+
     def __init__(self, title, window):
+        """Initialize WindowTitle."""
         super().__init__(window)
         self._game_title = title
 
     def draw(self):
+        """Draw the WindowTitle."""
         super().draw()
         (width, height) = self._window.get_size()
         font = pygame.font.get_default_font()
         window_font = pygame.font.Font(font, 70)
-        render_window = window_font.render(self._game_title, True, (0,0,0))
+        render_window = window_font.render(self._game_title, True, (0, 0, 0))
         position = render_window.get_rect(center=(width/2, 100))
         self._window.blit(render_window, position)
-    
