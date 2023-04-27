8e0d990c9b6f59e056cf5c72fc8154116884e5b9
Mario Linares <mariolinares@csu.fullerton.edu>
2023-04-26 23:43:36 -0700

    Add continous obstacle spawning

--- tankrunner/obstacle.py
+++ tankrunner/obstacle.py
@@ -10,15 +10,24 @@ class Obstacle():
         """Initialize the Obstacle."""
         self._surface = surface
         self._color = (255, 255, 255)
-        self._obstacle = pygame.Rect(600, 495, 40, 8)
-        self._obstacle_x_postion = self._obstacle.x
-        self._obstacle_y_positon = self._obstacle.y
+        self._obstacle = pygame.Rect(800, 495, 40, 8)
+        self._obstacle_copy = pygame.Rect(800, 495, 40, 8)
+        self._list = []
 
     def draw(self):
         """Draw the Tank."""
         pygame.draw.rect(self._surface, self._color, self._obstacle)
+        if self._obstacle.x < 0:
+            pygame.draw.rect(self._surface, self._color, self._obstacle_copy)
+            self._obstacle = pygame.Rect(800, 495, 40, 8)
+        if self._obstacle_copy.x < 0:
+            pygame.draw.rect(self._surface, self._color, self._obstacle)
+            self._obstacle_copy = pygame.Rect(800, 495, 40, 8)
 
     def move(self):
         """Control the Obstacle movement."""
         self._obstacle.x -= 4
-
+        if self._obstacle.x < 0:
+            self._obstacle_copy.x -= 4
+        if self._obstacle_copy.x < 0:
+            self._obstacle.x -= 4
--- tankrunner/window.py
+++ tankrunner/window.py
@@ -72,7 +72,7 @@ class WindowGame(Window):
         self._score = 0
         self._game_over = False
         self._tank = Tank(self._window)
-        self._obstacles = Obstacle(self._window)
+        self._obstacle = Obstacle(self._window)
 
     def draw(self):
         """Draw the game Window."""
@@ -85,7 +85,8 @@ class WindowGame(Window):
         floor = pygame.draw.line(self._window, (0, 0, 0), [0, 500], [800, 500], width=5)
 
         self._tank.draw()
-        self._obstacles.draw()
+        self._obstacle.draw()
+        
 
     def change_score(self):
         """Update the scoreboard."""
@@ -99,6 +100,6 @@ class WindowGame(Window):
     def update(self):
         """Update the game Window."""
         self._tank.move()
-        self._obstacles.move()
+        self._obstacle.move()
         if self._game_over != True:
             self.change_score()
